c4d995624ac6da7185ace00ae4e8680cb5385748
Thiago dos Santos Araújo <153017862+Thigas014@users.noreply.github.com>
2025-04-21 22:20:05 -0300

    Adição tela Login/Cadastro

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react'; 
-import { HashRouter, Routes, Route } from 'react-router-dom'; 
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 // Páginas
 import Home from './pages/Home.jsx';
 import History from './pages/History.jsx'
+import Login from './pages/Login.jsx';
 
-export default function App() {  
-  return ( 
+export default function App() {
+  return (
     <div>
       <HashRouter>
         <div className="content">
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/history' element={<History />} />
+            <Route path='/login' element={<Login />} />
+
           </Routes>
         </div>
       </HashRouter>
